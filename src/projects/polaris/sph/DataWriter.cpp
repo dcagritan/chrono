c78f610fbc50c16a53c1c5aa4e41dfffcc7235ae
dcagritan <dcagritan@gmail.com>
2022-09-29 11:58:46 +0200

    Box sizes have been changed

--- src/projects/polaris/sph/DataWriter.cpp
+++ src/projects/polaris/sph/DataWriter.cpp
@@ -230,11 +230,12 @@ DataWriterVehicle::DataWriterVehicle(ChSystemFsi& sysFSI, std::shared_ptr<Wheele
     // m_box_size.z() = 0.2;
     m_box_size.x() = 3.0 * tire_radius;
     m_box_size.y() = 2.5 * tire_width;
-    m_box_size.z() = 0.25;
+    m_box_size.z() = 0.2;
 
     // Set default offset of sampling box
-    // m_box_offset = ChVector<>(0.15, 0.0, 0.0);
-    m_box_offset = ChVector<>(0.0, 0.0, 0.0);
+    m_box_offset = ChVector<>(3.0 * tire_radius/2.0-0.5, 0.0, 0.0);
+    // m_box_offset = ChVector<>(0.0, 0.0, 0.0);
+    
 }
 
 void DataWriterVehicle::CollectDataMBS() {
