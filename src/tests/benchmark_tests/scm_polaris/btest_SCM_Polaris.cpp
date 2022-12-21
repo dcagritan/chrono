1714f2672e06faeefd5da160cebf0e484db4a5c1
dcagritan <dcagritan@gmail.com>
2022-12-21 17:13:56 +0100

    parts have been added

--- src/tests/benchmark_tests/scm_polaris/btest_SCM_Polaris.cpp
+++ src/tests/benchmark_tests/scm_polaris/btest_SCM_Polaris.cpp
@@ -27,312 +27,205 @@
 
 #include "chrono/physics/ChSystemNSC.h"
 #include "chrono/utils/ChUtilsInputOutput.h"
-// #include "chrono/utils/ChBenchmark.h"
+#include "chrono/utils/ChBenchmark.h"
 
-// #include "chrono_vehicle/ChVehicleModelData.h"
-// #include "chrono_vehicle/ChDriver.h"
-// #include "chrono_vehicle/terrain/SCMDeformableTerrain.h"
-// #include "chrono_vehicle/terrain/RigidTerrain.h"
-// #include "chrono_vehicle/wheeled_vehicle/utils/ChWheeledVehicleVisualSystemIrrlicht.h"
-
-// // #include "chrono/utils/ChBenchmark.h"
-
-// #include "chrono_models/vehicle/hmmwv/HMMWV.h"
-
-// #include "chrono_thirdparty/filesystem/path.h"
-
-// #include "DataWriter.h"
-// #include "CreateObjects.h"
-
-// using namespace chrono;
-// using namespace chrono::collision;
-// using namespace chrono::irrlicht;
-// using namespace chrono::vehicle;
-// using namespace chrono::vehicle::hmmwv;
-
-// using std::cout;
-// using std::endl;
-
-// // =============================================================================
-// // USER SETTINGS
-// // =============================================================================
-
-// // -----------------------------------------------------------------------------
-// // Terrain parameters
-// // -----------------------------------------------------------------------------
-
-// double terrainLength = 16.0;  // size in X direction
-// double terrainWidth = 8.0;    // size in Y direction
-// double delta = 0.05;          // SCM grid spacing
-
-// // -----------------------------------------------------------------------------
-// // Vehicle parameters
-// // -----------------------------------------------------------------------------
-
-// PolarisModel model = PolarisModel::MODIFIED;
-
-// // Type of tire (controls both contact and visualization)
-// enum class TireType { CYLINDRICAL, LUGGED };
-// TireType tire_type = TireType::LUGGED;
-
-// // Tire contact material properties
-// float Y_t = 1.0e6f;
-// float cr_t = 0.1f;
-// float mu_t = 0.8f;
-
-// // Initial vehicle position and orientation
-// ChVector<> initLoc(-3, 0, 0.6);
-// ChQuaternion<> initRot(1, 0, 0, 0);
-// ChCoordsys<> init_pos(initLoc, initRot);
-
-// // -----------------------------------------------------------------------------
-// // Simulation parameters
-// // -----------------------------------------------------------------------------
-
-// // Simulation step size
-// double step_size = 1e-3;
-
-// // Time interval between two render frames (1/FPS)
-// double render_step_size = 2.0 / 100;
-
-// // Point on chassis tracked by the camera
-// ChVector<> trackPoint(0.0, 0.0, 1.75);
-
-// // Output directories
-// const std::string out_dir = GetChronoOutputPath() + "POLARIS_SCM";
-// const std::string img_dir = out_dir + "/IMG";
-
-// // Visualization output
-// bool img_output = false;
-
-// // =============================================================================
-
-// class MyDriver : public ChDriver {
-//   public:
-//     MyDriver(ChVehicle& vehicle, double delay) : ChDriver(vehicle), m_delay(delay) {}
-//     ~MyDriver() {}
-
-//     virtual void Synchronize(double time) override {
-//         m_throttle = 0;
-//         m_steering = 0;
-//         m_braking = 1;
-
-//         double eff_time = time - m_delay;
-
-//         // Do not generate any driver inputs for a duration equal to m_delay.
-//         if (eff_time < 0)
-//             return;
-
-//         if (eff_time > 0.2)
-//         { 
-//             m_throttle = 0.7;
-//             m_braking = 0;
-//         }
-//         else
-//         { 
-//             m_throttle = 3.5 * eff_time;
-//             m_braking = 0;
-//         }
-
-//         if (eff_time < 2)
-//             m_steering = 0;
-//         else
-//             m_steering = 0.6 * std::sin(CH_C_2PI * (eff_time - 2) / 6);
-//     }
-
-//   private:
-//     double m_delay;
-// };
-
-// // template <int N>
-// // class ChainTest : public utils::ChBenchmarkTest {
-// //   public:
-// //     ChainTest();
-// //     ~ChainTest() { delete m_system; }
-
-// //     ChSystem* GetSystem() override { return m_system; }
-// //     void ExecuteStep() override { m_system->DoStepDynamics(m_step); }
-
-// //     void SimulateVis();
-
-// //   private:
-// //     ChSystem* m_system;
-// //     double m_length;
-// //     double m_step;
-// // };
-// // =============================================================================
-
-int main(int argc, char* argv[]) {
-//     GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
-//     // Parse command line arguments
-//     bool verbose = true;
-//     bool wheel_output = true;      // save individual wheel output files
-//     double output_major_fps = 50;
-//     // --------------------
-//     // Create the Chrono systems
-//     // --------------------
-//     ChSystemNSC sys;
-//     sys.SetNumThreads(std::min(8, ChOMP::GetNumProcs()));
-//     const ChVector<> gravity(0, 0, -9.81);
-//     sys.Set_G_acc(gravity);
-
-//     // --------------------
-//     // Create the Polaris vehicle
-//     // --------------------
-//     cout << "Create vehicle..." << endl;
-//     auto vehicle = CreateVehicle(model, sys, init_pos);
-
-//     // ------------------
-//     // Create the terrain
-//     // ------------------
-//     // SCMDeformableTerrain terrain(system);
-//     SCMDeformableTerrain terrain(&sys);
-//     terrain.SetSoilParameters(2e6,   // Bekker Kphi
-//                                 0,     // Bekker Kc
-//                                 1.1,   // Bekker n exponent
-//                                 0,     // Mohr cohesive limit (Pa)
-//                                 30,    // Mohr friction limit (degrees)
-//                                 0.01,  // Janosi shear coefficient (m)
-//                                 2e8,   // Elastic stiffness (Pa/m), before plastic yield
-//                                 3e4    // Damping (Pa s/m), proportional to negative vertical speed (optional)
-//     );
-
-//     ////terrain.EnableBulldozing(true);      // inflate soil at the border of the rut
-//     ////terrain.SetBulldozingParameters(55,   // angle of friction for erosion of displaced material at rut border
-//     ////                                0.8,  // displaced material vs downward pressed material.
-//     ////                                5,    // number of erosion refinements per timestep
-//     ////                                10);  // number of concentric vertex selections subject to erosion
-
-//     // Optionally, enable moving patch feature (single patch around vehicle chassis)
-//     // terrain.AddMovingPatch(my_hmmwv.GetChassisBody(), ChVector<>(0, 0, 0), ChVector<>(5, 3, 1));
-
-//     // Optionally, enable moving patch feature (multiple patches around each wheel)
-//     ////for (auto& axle : my_hmmwv.GetVehicle().GetAxles()) {
-//     ////    terrain.AddMovingPatch(axle->m_wheels[0]->GetSpindle(), ChVector<>(0, 0, 0), ChVector<>(1, 0.5, 1));
-//     ////    terrain.AddMovingPatch(axle->m_wheels[1]->GetSpindle(), ChVector<>(0, 0, 0), ChVector<>(1, 0.5, 1));
-//     ////}
-
-//     ////terrain.SetTexture(vehicle::GetDataFile("terrain/textures/grass.jpg"), 80, 16);
-//     ////terrain.SetPlotType(vehicle::SCMDeformableTerrain::PLOT_PRESSURE_YELD, 0, 30000.2);
-//     terrain.SetPlotType(vehicle::SCMDeformableTerrain::PLOT_SINKAGE, 0, 0.1);
-
-//     terrain.Initialize(terrainLength, terrainWidth, delta);
+#include "chrono_vehicle/ChVehicleModelData.h"
+#include "chrono_vehicle/ChDriver.h"
+#include "chrono_vehicle/terrain/SCMDeformableTerrain.h"
+#include "chrono_vehicle/terrain/RigidTerrain.h"
+#include "chrono_vehicle/wheeled_vehicle/utils/ChWheeledVehicleVisualSystemIrrlicht.h"
 
 
-//     // std::string vertices_filename = out_dir +  "/vertices_" + std::to_string(0) + ".csv";
-//     // terrain.WriteMeshVertices(vertices_filename);
+#include "chrono_models/vehicle/hmmwv/HMMWV.h"
 
-//     // ---------------------------------------
-//     // Create the vehicle Irrlicht application
-//     // ---------------------------------------
-//     auto vis = chrono_types::make_shared<ChWheeledVehicleVisualSystemIrrlicht>();
-//     vis->SetWindowTitle("HMMWV Deformable Soil Demo");
-//     vis->SetChaseCamera(trackPoint, 6.0, 0.5);
+#include "chrono_thirdparty/filesystem/path.h"
+
+#include "CreateObjects.h"
+
+using namespace chrono;
+using namespace chrono::collision;
+using namespace chrono::irrlicht;
+using namespace chrono::vehicle;
+using namespace chrono::vehicle::hmmwv;
+
+using std::cout;
+using std::endl;
+
+// =============================================================================
+// USER SETTINGS
+// =============================================================================
+
+// -----------------------------------------------------------------------------
+// Terrain parameters
+// -----------------------------------------------------------------------------
+
+double terrainLength = 16.0;  // size in X direction
+double terrainWidth = 8.0;    // size in Y direction
+double delta = 0.05;          // SCM grid spacing
+
+// -----------------------------------------------------------------------------
+// Vehicle parameters
+// -----------------------------------------------------------------------------
+
+PolarisModel model = PolarisModel::MODIFIED;
+
+// Type of tire (controls both contact and visualization)
+enum class TireType { CYLINDRICAL, LUGGED };
+TireType tire_type = TireType::LUGGED;
+
+// Tire contact material properties
+float Y_t = 1.0e6f;
+float cr_t = 0.1f;
+float mu_t = 0.8f;
+
+// Initial vehicle position and orientation
+ChVector<> initLoc(-3, 0, 0.6);
+ChQuaternion<> initRot(1, 0, 0, 0);
+ChCoordsys<> init_pos(initLoc, initRot);
+
+// -----------------------------------------------------------------------------
+// Simulation parameters
+// -----------------------------------------------------------------------------
+
+// Simulation step size
+double step_size = 1e-3;
+
+// Time interval between two render frames (1/FPS)
+double render_step_size = 2.0 / 100;
+
+// Point on chassis tracked by the camera
+ChVector<> trackPoint(0.0, 0.0, 1.75);
+
+// Output directories
+const std::string out_dir = GetChronoOutputPath() + "POLARIS_SCM";
+const std::string img_dir = out_dir + "/IMG";
+
+// Visualization output
+bool img_output = false;
+
+// =============================================================================
+
+class MyDriver : public ChDriver {
+  public:
+    MyDriver(ChVehicle& vehicle, double delay) : ChDriver(vehicle), m_delay(delay) {}
+    ~MyDriver() {}
+
+    virtual void Synchronize(double time) override {
+        m_throttle = 0;
+        m_steering = 0;
+        m_braking = 1;
+
+        double eff_time = time - m_delay;
+
+        // Do not generate any driver inputs for a duration equal to m_delay.
+        if (eff_time < 0)
+            return;
+
+        if (eff_time > 0.2)
+        { 
+            m_throttle = 0.7;
+            m_braking = 0;
+        }
+        else
+        { 
+            m_throttle = 3.5 * eff_time;
+            m_braking = 0;
+        }
+
+        if (eff_time < 2)
+            m_steering = 0;
+        else
+            m_steering = 0.6 * std::sin(CH_C_2PI * (eff_time - 2) / 6);
+    }
+
+  private:
+    double m_delay;
+};
+
+template <int N>
+class SCMPolarisTest : public utils::ChBenchmarkTest {
+  public:
+    SCMPolarisTest();
+    ~SCMPolarisTest() { delete m_system; }
+
+    ChSystem* GetSystem() override { return m_system; }
+    void ExecuteStep() override { m_system->DoStepDynamics(m_step); }
+
+    // void SimulateVis();
+
+  private:
+    ChSystem* m_system;
+    double m_length;
+    double m_step;
+};
+
+template <int N>
+SCMPolarisTest<N>::SCMPolarisTest() : m_length(0.25), m_step(1e-3) {
+    // Parse command line arguments
+    // bool verbose = true;
+    // bool wheel_output = true;      // save individual wheel output files
+    // double output_major_fps = 50;
+    // std::cout<<"Hhh"<<std::endl;
+    // // --------------------
+    // // Create the Chrono systems
+    // // --------------------
+    m_system= new ChSystemNSC;
+    // m_system->SetNumThreads(std::min(8, ChOMP::GetNumProcs()));
+    // const ChVector<> gravity(0, 0, -9.81);
+    // m_system->Set_G_acc(gravity);
+
+    // // --------------------
+    // // Create the Polaris vehicle
+    // // --------------------
+    // cout << "Create vehicle..." << endl;
+    // auto vehicle = CreateVehicle(model, *m_system, init_pos);
+}
+
+// template <int N>
+// void SCMPolarisTest<N>::SimulateVis() {
+// #ifdef CHRONO_IRRLICHT
+//     double offset = N * m_length;
+
+//     // Create the Irrlicht visualization system
+//     auto vis = chrono_types::make_shared<irrlicht::ChVisualSystemIrrlicht>();
+//     vis->AttachSystem(m_system);
+//     vis->SetWindowSize(800, 600);
+//     vis->SetWindowTitle("Pendulum chain");
 //     vis->Initialize();
-//     vis->AddLightDirectional();
-//     vis->AddSkyBox();
 //     vis->AddLogo();
-//     ChVehicle* castedvehicle = vehicle.get();
-//     // auto castedvehicle = std::static_pointer_cast<ChVehicle>(vehicle);
-//     // ChVehicle* castedvehicle = (ChVehicle*)vehicle
-//     vis->AttachVehicle(castedvehicle);
-
-//     // --------------------
-//     // Create driver system
-//     // --------------------
-//     MyDriver driver(*castedvehicle, 0.5);
-//     driver.Initialize();
-
-//     // -----------------
-//     // Initialize output
-//     // -----------------
-//     if (!filesystem::create_directory(filesystem::path(out_dir))) {
-//         std::cout << "Error creating directory " << out_dir << std::endl;
-//         return 1;
-//     }
-//     if (img_output) {
-//         if (!filesystem::create_directory(filesystem::path(img_dir))) {
-//             std::cout << "Error creating directory " << img_dir << std::endl;
-//             return 1;
-//         }
-//     }
-
-//     DataWriterVehicle data_writer(&sys, vehicle, terrain);
-//     data_writer.SetVerbose(verbose);
-//     data_writer.SetMBSOutput(wheel_output);
-//     data_writer.Initialize(out_dir, output_major_fps, step_size);
-//     cout << "Simulation output data saved in: " << out_dir << endl;
-//     cout << "===============================================================================" << endl;
-
-//     // ---------------
-//     // Simulation loop
-//     // ---------------
-//     std::cout << "Total vehicle mass: " << vehicle->GetMass() << std::endl;
-
-//     // Solver settings.
-//     sys.SetSolverMaxIterations(50);
-
-//     // Number of simulation steps between two 3D view render frames
-//     int render_steps = (int)std::ceil(render_step_size / step_size);
-
-//     // Initialize simulation frame counter
-//     int step_number = 0;
-//     int render_frame = 0;
-
-//     ChTimer<> timer;
+//     vis->AddSkyBox();
+//     vis->AddTypicalLights();
+//     vis->AddCamera(ChVector<>(0, -offset / 2, offset), ChVector<>(0, -offset / 2, 0));
 
 //     while (vis->Run()) {
-//         double time = sys.GetChTime();
-
-//         if (step_number == 800) {
-//             std::cout << "\nstart timer at t = " << time << std::endl;
-//             timer.start();
-//         }
-//         if (step_number == 1400) {
-//             timer.stop();
-//             std::cout << "stop timer at t = " << time << std::endl;
-//             std::cout << "elapsed: " << timer() << std::endl;
-//             std::cout << "\nSCM stats for last step:" << std::endl;
-//             terrain.PrintStepStatistics(std::cout);
-//         }
-
-//         // Render scene
 //         vis->BeginScene();
 //         vis->Render();
-//         tools::drawColorbar(vis.get(), 0, 0.1, "Sinkage", 30);
+//         m_system->DoStepDynamics(m_step);
 //         vis->EndScene();
-
-//         data_writer.Process(step_number, time);
-
-//         if (step_number % render_steps == 0) {
-//             std::string vertices_filename = out_dir +  "/vertices_" + std::to_string(render_frame) + ".csv";
-//             terrain.WriteMeshVertices(vertices_filename);
-//             std::cout<<"Simulation time= "<<step_number*step_size<<std::endl;
-//             if (img_output% render_steps == 0)
-//             {
-//             char filename[100];
-//             sprintf(filename, "%s/img_%03d.jpg", img_dir.c_str(), render_frame + 1);
-//             vis->WriteImageToFile(filename);
-//             }
-//             render_frame++;
-//         }
-
-//         // // Driver inputs
-//         DriverInputs driver_inputs = driver.GetInputs();
-
-//         // // Update modules
-//         driver.Synchronize(time);
-//         terrain.Synchronize(time);
-//         vehicle->Synchronize(time, driver_inputs, terrain);
-//         vis->Synchronize("", driver_inputs);
-
-//         // Advance dynamics
-//         sys.DoStepDynamics(step_size);
-//         vis->Advance(step_size);
-
-//         // Increment frame number
-//         step_number++;
 //     }
+// #endif
+// }
 
-    return 0;
+// =============================================================================
+
+#define NUM_SKIP_STEPS 2000  // number of steps for hot start
+#define NUM_SIM_STEPS 1000   // number of simulation steps for each benchmark
+
+CH_BM_SIMULATION_LOOP(SCMPolaris04, SCMPolarisTest<4>,  NUM_SKIP_STEPS, NUM_SIM_STEPS, 5);
+
+
+// =============================================================================
+
+int main(int argc, char* argv[]) {
+    ::benchmark::Initialize(&argc, argv);
+
+// #ifdef CHRONO_IRRLICHT
+//     if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
+//         SCMPolarisTest<4> test;
+//         test.SimulateVis();
+//         return 0;
+//     }
+// #endif
+
+    ::benchmark::RunSpecifiedBenchmarks();
 }
