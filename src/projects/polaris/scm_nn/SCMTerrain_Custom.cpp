23ddb27beac16fc5b5ef41151ac25e84bfd5660b
dcagritan <dcagritan@gmail.com>
2023-05-03 19:47:25 +0200

    SCMTerrain_Custom has been created

---
+++ src/projects/polaris/scm_nn/SCMTerrain_Custom.cpp
@@ -0,0 +1,53 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Alessandro Tasora, Radu Serban, Jay Taves
+// =============================================================================
+//
+// Deformable terrain based on SCM (Soil Contact Model) from DLR
+// (Krenn & Hirzinger)
+//
+// =============================================================================
+
+#include <cstdio>
+#include <cmath>
+#include <queue>
+#include <unordered_set>
+#include <limits>
+
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
+
+#include "chrono/physics/ChMaterialSurfaceNSC.h"
+#include "chrono/physics/ChMaterialSurfaceSMC.h"
+#include "chrono/assets/ChTexture.h"
+#include "chrono/assets/ChBoxShape.h"
+#include "chrono/utils/ChConvexHull.h"
+
+#include "chrono_vehicle/ChVehicleModelData.h"
+#include "SCMTerrain_Custom.h"
+
+#include "chrono_thirdparty/stb/stb.h"
+
+namespace chrono {
+namespace vehicle {
+
+// -----------------------------------------------------------------------------
+// Implementation of the SCMTerrain wrapper class
+// -----------------------------------------------------------------------------
+
+SCMTerrain_Custom::SCMTerrain_Custom(ChSystem* system, bool visualization_mesh) {
+}
+
+
+}  // end namespace vehicle
+}  // end namespace chrono
---
+++ src/projects/polaris/scm_nn/SCMTerrain_Custom.h
@@ -0,0 +1,67 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Alessandro Tasora, Radu Serban, Jay Taves
+// =============================================================================
+//
+// Deformable terrain based on SCM (Soil Contact Model) from DLR
+// (Krenn & Hirzinger)
+//
+// =============================================================================
+#pragma once
+
+#include <string>
+#include <ostream>
+#include <unordered_map>
+
+#include "chrono/assets/ChTriangleMeshShape.h"
+#include "chrono/physics/ChBody.h"
+#include "chrono/physics/ChLoadContainer.h"
+#include "chrono/physics/ChLoadsBody.h"
+#include "chrono/physics/ChSystem.h"
+#include "chrono/core/ChTimer.h"
+
+#include "chrono_vehicle/ChApiVehicle.h"
+#include "chrono_vehicle/ChSubsysDefs.h"
+#include "chrono_vehicle/ChTerrain.h"
+#include "chrono_vehicle/ChWorldFrame.h"
+#include "chrono_vehicle/terrain/SCMTerrain.h"
+
+namespace chrono {
+namespace vehicle {
+
+
+/// @addtogroup vehicle_terrain
+/// @{
+
+/// Deformable terrain model.
+/// This class implements a deformable terrain based on the Soil Contact Model.
+/// Unlike RigidTerrain, the vertical coordinates of this terrain mesh can be deformed
+/// due to interaction with ground vehicles or other collision shapes.
+class CH_VEHICLE_API SCMTerrain_Custom : public ChTerrain {
+  public:
+
+    /// Construct a default SCM deformable terrain.
+    /// The user is responsible for calling various Set methods before Initialize.
+    SCMTerrain_Custom(ChSystem* system,               ///< [in] containing multibody system
+               bool visualization_mesh = true  ///< [in] enable/disable visualization asset
+    );
+
+    ~SCMTerrain_Custom() {}
+
+};
+
+
+/// @} vehicle_terrain
+
+}  // end namespace vehicle
+}  // end namespace chrono
+
